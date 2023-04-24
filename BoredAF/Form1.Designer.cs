3b050c1ed27e6bc8b5ea44a648747d3756719e3c
HasanSuhail47 <hasansuhail007@gmail.com>
2023-04-25 04:03:11 +0500

    Final

---
+++ BoredAF/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace BoredAF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gametimer = new System.Windows.Forms.Timer(this.components);
+            this.player = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Background = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Background)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gametimer
+            // 
+            this.gametimer.Enabled = true;
+            this.gametimer.Interval = 5;
+            this.gametimer.Tick += new System.EventHandler(this.Maintimer);
+            // 
+            // player
+            // 
+            this.player.Image = global::BoredAF.Properties.Resources.player2_01;
+            this.player.Location = new System.Drawing.Point(438, 78);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(62, 72);
+            this.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.player.TabIndex = 3;
+            this.player.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // Background
+            // 
+            this.Background.Location = new System.Drawing.Point(0, 0);
+            this.Background.Name = "Background";
+            this.Background.Size = new System.Drawing.Size(650, 500);
+            this.Background.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Background.TabIndex = 0;
+            this.Background.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(634, 511);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.player);
+            this.Controls.Add(this.Background);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Keydown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Keyup);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Background)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer gametimer;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox Background;
+    }
+}
+
---
+++ BoredAF/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace BoredAF
+{
+    public partial class Form1 : Form
+    {
+        bool left, right, jump,interaction_bottom,interaction_right,interaction_left,interaction_up;
+        int playerspeed = 5;
+        double ga = 0.5;
+        double gs = 0;
+        int jumpforce = 20;
+        int counter = 0;
+       
+        
+        PictureBox thrust = new PictureBox();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+       
+
+        private void Keydown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                left = true;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                right = true;
+            }
+            if (e.KeyCode == Keys.Space && jump==false)
+            {
+                jump = true;
+            }
+            if (e.KeyCode == Keys.X && jump == false)
+            {
+                player.Location = new Point(0, 0);
+            }
+
+        }
+
+        private void Keyup(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                left = false;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                right = false;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            thrust.Name = "thrust";
+            thrust.Size = new Size(30, 60);
+            thrust.Location = new Point(player.Location.X, player.Location.Y + (int)player.Height);
+            thrust.Image = Image.FromFile(@"C:\Users\Home PC\source\repos\BoredAF\BoredAF\Pictures\thrust-01.png");
+            this.Controls.Add(thrust);
+            thrust.SizeMode= System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            thrust.BringToFront();
+            thrust.Hide();
+            this.levelbuilder();
+            
+            player.Image = Image.FromFile(@"C:\Users\Home PC\source\repos\BoredAF\BoredAF\Pictures\player-01.png");
+            player.BackColor = Color.Transparent;
+           
+        }
+
+        private void movement(string direction)
+        {
+            
+        }
+
+        private void Maintimer(object sender, EventArgs e)
+        {
+            
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox && (string)x.Tag == "platform")
+                {
+                    if (player.Bounds.Bottom < x.Bounds.Top+5&& player.Bounds.Bottom> x.Bounds.Top - 5&&
+                        player.Bounds.Left>x.Bounds.Left&& player.Bounds.Left < x.Bounds.Right||
+                        player.Bounds.Bottom < x.Bounds.Top + 5 && player.Bounds.Bottom > x.Bounds.Top - 5 &&
+                        player.Bounds.Right > x.Bounds.Left && player.Bounds.Right < x.Bounds.Right)
+                    {
+                       
+                        gs = 0;
+                    }
+                    
+                    if (player.Bounds.Left < x.Bounds.Right + 5 && player.Bounds.Left > x.Bounds.Right - 5 &&
+                        player.Bounds.Bottom < x.Bounds.Bottom && player.Bounds.Bottom > x.Bounds.Top ||
+                        player.Bounds.Left < x.Bounds.Right + 5 && player.Bounds.Left > x.Bounds.Right - 5 &&
+                        player.Bounds.Top < x.Bounds.Bottom && player.Bounds.Top > x.Bounds.Top
+                        )
+                    {
+
+                        left = false;
+                    }
+                    
+                    if (player.Bounds.Right < x.Bounds.Left + 5 && player.Bounds.Right > x.Bounds.Left - 5 &&
+                        player.Bounds.Bottom < x.Bounds.Bottom && player.Bounds.Bottom > x.Bounds.Top ||
+                        player.Bounds.Right < x.Bounds.Left + 5 && player.Bounds.Right > x.Bounds.Left - 5 &&
+                        player.Bounds.Top < x.Bounds.Bottom && player.Bounds.Top > x.Bounds.Top
+                        )
+                    {
+                        right = false;
+                    }
+                    
+
+                }
+
+            }
+            
+            player.Location = new Point(player.Location.X, player.Location.Y + (int)gs);
+
+            if (gs < 5)
+            { 
+                thrust.Hide();
+                gs += ga;
+            }
+            else
+            {
+                thrust.Image = Image.FromFile(@"C:\Users\Home PC\source\repos\BoredAF\BoredAF\Pictures\thrust-02.png");
+                thrust.Location = new Point(player.Location.X+15, player.Location.Y + (int)player.Height);
+                thrust.Show();
+            }
+                
+            
+
+            if (jump == true && jumpforce >= 0)
+            {
+                
+                player.Location = new Point(player.Location.X, player.Location.Y - jumpforce);
+                jumpforce -= 1;
+                interaction_bottom = false;
+                thrust.Image = Image.FromFile(@"C:\Users\Home PC\source\repos\BoredAF\BoredAF\Pictures\thrust-01.png");
+                thrust.Location = new Point(player.Location.X + 15, player.Location.Y + (int)player.Height);
+                thrust.Show();
+            }
+            else
+            {
+                jumpforce = 20;
+                jump= false;
+            }
+
+            if (left == true && player.Left > 3&& interaction_left==false  )
+            {
+                player.Image = Image.FromFile(@"C:\Users\Home PC\source\repos\BoredAF\BoredAF\Pictures\player-01.png");
+                player.Location = new Point(player.Location.X - playerspeed, player.Location.Y);
+            }
+            if (right == true && player.Left < Background.Width - player.Width)
+            {
+                player.Image = Image.FromFile(@"C:\Users\Home PC\source\repos\BoredAF\BoredAF\Pictures\player2-01.png");
+                player.Location = new Point(player.Location.X + playerspeed, player.Location.Y);
+            }
+        }
+
+        private void levelbuilder()
+        {
+            string[] s= File.ReadAllLines(@"C:\Users\Home PC\source\repos\BoredAF\BoredAF\Pictures\Map1.txt");
+            
+            this.Size = new Size(Int32.Parse((s[2].Split('|'))[0]), Int32.Parse((s[2].Split('|'))[1])+40);
+            Background.Name = s[0];
+            Background.Image = Image.FromFile(s[1]);
+            Background.Size = new Size(Int32.Parse((s[2].Split('|'))[0]), Int32.Parse((s[2].Split('|'))[1]));
+            Background.Location= new Point(Int32.Parse((s[3].Split('|'))[0]),Int32.Parse((s[3].Split('|'))[1]));
+            Background.Tag = s[4];
+            Background.SizeMode=PictureBoxSizeMode.StretchImage;
+
+            int i=9;
+            for (; i < s.Length; i++)
+            {
+                int location= Int32.Parse(s[i].Split(':')[0].Split('/')[0])*50;
+                for (int j = 0; j < Int32.Parse(s[i].Split(':')[1]);j++)
+                {
+                    PictureBox p=new PictureBox
+                    {
+                        Name = s[5],
+                        Image = Image.FromFile(s[6]),
+                        Size= new Size(Int32.Parse((s[7].Split('|'))[0]),Int32.Parse((s[7].Split('|'))[1])),
+                        Tag = s[8],
+                        Location = new Point(location, Int32.Parse(s[i].Split(':')[0].Split('/')[1])*50),
+                        SizeMode=PictureBoxSizeMode.StretchImage
+                    };
+                    location += 50;
+                    this.Controls.Add(p);
+                    p.BringToFront();
+                }
+
+            }
+        }
+    }
+}
